c6ea2431d193b3ac57874135bb847d2cf7bbcb14
Trevor Clarke <trevor.clarke62@gmail.com>
2020-01-19 22:28:39 -0500

    Create Removed_Any_Applied_Entries.js

---
+++ Removed_Any_Applied_Entries.js
@@ -0,0 +1,7 @@
+var elements = document.querySelectorAll('a[role="button"]');
+for(var i = 0; i<elements.length; i++){
+	var elementText = elements[i].innerHTML;
+  if(elementText.trim() == "Applied"){
+  	elements[i].parentNode.parentNode.style.display = "none";
+  }
+}
