686a72b507d88b9ab122b520444b5b7788fca20c
Jacky Ly <lyjacky@live.com>
2020-01-21 11:40:15 -0500

    Update Remove_Any_Applied_Entries.js

--- Remove_Any_Applied_Entries.js
+++ Remove_Any_Applied_Entries.js
@@ -1,7 +1,11 @@
 var elements = document.querySelectorAll('a[role="button"]');
+removeItems();
+function removeItems() {
 for(var i = 0; i<elements.length; i++){
 	var elementText = elements[i].innerHTML;
   if(elementText.trim() == "Applied"){
   	elements[i].parentNode.parentNode.style.display = "none";
   }
+}
+setTimeout(removeItems, 500);
 }
